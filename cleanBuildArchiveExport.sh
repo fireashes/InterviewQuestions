4708f0261c61dd4c69e579dc9bcb5407cb095b60
Fire Ashes <fire3ashes@gmail.com>
2017-03-13 15:39:30 -0500

    cleanup

--- cleanBuildArchiveExport.sh
+++ cleanBuildArchiveExport.sh
@@ -3,13 +3,14 @@ project=InterviewQuestions
 rm -rfv build
 mkdir build
 xcodebuild -list | tee build/list.txt
-for scheme in "InterviewQuestionsFree" "InterviewQuestionsFull"; do
+for schemeprefix in "InterviewQuestionsFree" "InterviewQuestionsFull"; do
 for configuration in "Debug" "Release"; do
-	archivePath=build/${scheme}${configuration}.xcarchive
-	exportPath=build/${scheme}${configuration}.ipa
-	xcodebuild -scheme ${scheme} -configuration ${configuration} -showBuildSettings | tee build/${scheme}${configuration}.xcconfig
-	xcodebuild -scheme ${scheme} -configuration ${configuration} -archivePath ${archivePath} -verbose clean build archive -IDEBuildOperationMaxNumberOfConcurrentCompileTasks=1 | tee build/cleanBuildArchive${scheme}${configuration}.txt
-	xcodebuild -archivePath ${archivePath} -exportArchive -exportFormat IPA -exportPath ${exportPath} -verbose -IDEBuildOperationMaxNumberOfConcurrentCompileTasks=1 | tee build/exportArchive${scheme}${configuration}.txt
+	scheme=${schemeprefix}${configuration}
+	archivePath=build/${scheme}.xcarchive
+	exportPath=build/${scheme}.ipa
+	xcodebuild -scheme ${scheme} -configuration ${configuration} -showBuildSettings | tee build/${scheme}.xcconfig
+	xcodebuild -scheme ${scheme} -configuration ${configuration} -archivePath ${archivePath} -verbose clean build archive -IDEBuildOperationMaxNumberOfConcurrentCompileTasks=1 | tee build/cleanBuildArchive${scheme}.txt
+	xcodebuild -archivePath ${archivePath} -exportArchive -exportFormat IPA -exportPath ${exportPath} -verbose -IDEBuildOperationMaxNumberOfConcurrentCompileTasks=1 | tee build/exportArchive${scheme}.txt
 done
 done
 )
